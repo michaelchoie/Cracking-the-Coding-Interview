aac0b303fa45eb47d59d80e449bc3e20334226b2
Michael Choie <MichaelChoie@Michaels-MacBook-Pro-2.local>
2018-06-22 13:44:59 -0400

    Adding completed exercises

--- Chapter 3/4. Queue via Stacks.py
+++ Chapter 3/4. Queue via Stacks.py
@@ -5,11 +5,42 @@ from Stack import Stack
 
 class Queue(object):
 
-    pass
+    def __init__(self, size):
+        self.input_stack = Stack(size)
+        self.output_stack = Stack(size)
+
+    def __str__(self):
+        stack1 = str(self.input_stack)
+        stack2 = str(self.output_stack)
+        return ', '.join([stack1, stack2])
+
+    def enqueue(self, value):
+        self.input_stack.push(value)
+
+    def dequeue(self):
+        if self.input_stack.is_empty() and self.output_stack.is_empty():
+            raise Exception("Queue is empty")
+
+        if self.output_stack.is_empty():
+            while not self.input_stack.is_empty():
+                value = self.input_stack.pop()
+                self.output_stack.push(value)
+
+        value = self.output_stack.pop()
+
+        return value
 
 
 class Test(unittest.TestCase):
-    pass
+
+    def test_enqueue_dequeue(self):
+        queue = Queue(5)
+
+        for x in range(5):
+            queue.enqueue(x)
+
+        for x in range(5):
+            self.assertEqual(queue.dequeue(), x)
 
 
 if __name__ == "__main__":
